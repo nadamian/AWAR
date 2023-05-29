0baae66de7029f13f3c48c5614d669aacb9f99f1
Nathaniel Adamian <nat.adamian241@gmail.com>
2023-05-29 16:30:54 -0400

    Run expectancy matrix generating
    Matrices generating correctly via two different methods

--- DataParser.py
+++ DataParser.py
@@ -8,10 +8,7 @@ import os
 # int inputs: 2,3,4,8,9,32-34,37,40,43,46,51-52,54,56,58-65
 # bevent -y 2022 -f 0,26-31,35-36,38-39,41-42,44-45,47-50,53,55,57,66-74 2022ANA.eva > 2022ANASTR.csv
 # bevent -y 2022 -f 2,3,4,8,9,32-34,37,40,43,46,51-52,54,56,58-65 2022ANA.eva > 2022ANAINT.csv
-INT_PATH = r'C:\Users\natad\PycharmProjects\AWAR\Data\Output\2022INT.csv'
-STR_PATH = r'C:\Users\natad\PycharmProjects\AWAR\Data\Output\2022STR.csv'
-GAME_PATH = r'C:\Users\natad\PycharmProjects\AWAR\Data\Output\2019GAME.csv'
-OUTPUT_DIR = r'C:\Users\natad\PycharmProjects\AWAR\Data\Output'
+BASE_DATA_PATH = os.path.join(os.getcwd(), 'Data', 'Output')
 
 
 def read_file(path_str: str, path_int: str):
@@ -19,12 +16,16 @@ def read_file(path_str: str, path_int: str):
     int_data = np.genfromtxt(path_int, delimiter=',', dtype=int)
     return str_data, int_data,
 
-
-if __name__ == '__main__':
+def generate_expectancy_matrix(year: str):
     t = time.time()
-    str_data, int_data = read_file(STR_PATH, INT_PATH)
+    str_path = os.path.join(BASE_DATA_PATH, year+'STR.csv')
+    int_path = os.path.join(BASE_DATA_PATH, year+'INT.csv')
+    str_data, int_data = read_file(str_path, int_path)
     outs_scores = RunExpectancy.stitch_data(str_data, int_data)
     matrix = RunExpectancy.build_run_ex_matrix(outs_scores)
-    np.savetxt(os.path.join(OUTPUT_DIR, "2022RunMatrix.csv"), matrix.T)
+    np.savetxt(os.path.join(BASE_DATA_PATH, year + 'RunMatrix.csv'), matrix.T)
     endtime = time.time()-t
-    print("Runtime: " + str(endtime))
+    print('Runtime: ' + str(endtime))
+
+if __name__ == '__main__':
+    generate_expectancy_matrix('2019')
--- RunExpectancy.py
+++ RunExpectancy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import DataParser as parser
 # Column indices of data in int data matrix
 INNING_NUM_INDEX = 0
@@ -6,47 +7,40 @@ BTEAM_INDEX = 1
 OUTS_INDEX = 2
 VIS_SCORE = 3
 HOME_SCORE = 4
-RBI_INDEX = 10
+BAT_DEST = 16
+FIRST_DEST = 17
+SECOND_DEST = 18
+THIRD_DEST = 19
 # Column indices of data in str data matrix
 ON_FIRST = 1
 ON_SECOND = 2
 ON_THIRD = 3
 GAME_END = 31
+BASE_DATA_PATH = os.path.join(os.getcwd(), 'Data', 'Output')
 
-# TODO sum runs and roll back by one
-
-def expectancy(str_data: np.ndarray, int_data):
-    runners_on_probs = runners_on_base(str_data)
-
-
-def runners_on_base(str_data: np.ndarray):
-    """Returns an array with the probability that there is a runner on each base"""
-    runners_on = str_data[:, [ON_FIRST, ON_SECOND, ON_THIRD]]
-    runners_on[runners_on == '""'] = 0
-    runners_on[runners_on != '0'] = 1
-    runners_on_float = runners_on.astype(float)
-    means = np.mean(runners_on_float, axis=0)
-    return means
-
-
-def scores_from_prob(int_data: np.ndarray):
-    evt_data = int_data[:, [2, 12, 13, 14]]
-
-
+"""Run expectancy matrix and helper methods"""
 def build_run_ex_matrix(outs_scores: np.ndarray):
     """Matrix Structure:
     0 outs | 1 out | 2 outs
     empty, 1B, 2B, 3B, 1B2B, 1B3B, 2B3B, 1B2B3B"""
     base_matrix = np.zeros((3, 8))
     situation_instances = np.zeros(base_matrix.shape)
-    score_on_play = np.roll(outs_scores[:, 3], -1) - outs_scores[:, 3]
+    """score_on_play = np.roll(outs_scores[:, 3], -1) - outs_scores[:, 3]
     score_on_play[np.where(score_on_play < 0)] = 0
-    outs_scores[:, 3] = score_on_play
+    outs_scores[:, 3] = score_on_play"""
+    outs_scores = play_score(outs_scores)
     split_indices = np.where(np.logical_or(outs_scores[:, 1][:-1] != outs_scores[:, 1][1:], outs_scores[:, 0][:-1] != outs_scores[:, 0][1:]))[0] + 1
     innings = np.split(outs_scores, split_indices)
+    scores = get_game_scores()
+    game_index = 0
     for inning in innings:
-        if (np.isin(1, inning[:, 7]) or inning[0, 0] >= 9) and (int(inning[0, 1]) == 1): # alt: check_walk_off(games_final, inning, game_index)
+        if (np.isin(1, inning[:, 11]) or inning[0, 0] >= 9) and (int(inning[0, 1]) == 1): # alt: check_walk_off(games_final, inning, game_index)
             continue
+        """if np.isin(1, inning[:, 11]):
+            if scores[game_index] != inning[inning.shape[0]-1, 3]:
+                game_index += 1
+                continue
+            game_index +=1"""
         matrix, instances= run_ex_inning(inning)
         base_matrix = np.add(base_matrix, matrix)
         situation_instances = np.add(situation_instances, instances)
@@ -59,10 +53,11 @@ def run_ex_inning(inning: np.ndarray):
     instances = np.zeros(base_matrix.shape)
     for event in inning:
         outs = int(event[2])
-        values = (int(event[4]), int(event[5]), int(event[6]))
+        values = (int(event[8]), int(event[9]), int(event[10]))
         occupied_bases = S_3_element(values)
         instances[outs, occupied_bases] += 1
-        base_matrix += event[3] * instances
+        # Event[3] or event[4] depending on scoring style
+        base_matrix += event[4] * instances
     return base_matrix, instances
 
 
@@ -81,8 +76,20 @@ def stitch_data(str_data: np.ndarray, int_data: np.ndarray):
     runners_on[runners_on != '0'] = 1
     runners_on_float = runners_on.astype(float)
     int_data[:, VIS_SCORE] += int_data[:, HOME_SCORE]
-    outs_scores = int_data[:, [INNING_NUM_INDEX, BTEAM_INDEX, OUTS_INDEX, VIS_SCORE]]
+    outs_scores = int_data[:, [INNING_NUM_INDEX, BTEAM_INDEX, OUTS_INDEX, VIS_SCORE, BAT_DEST, FIRST_DEST, SECOND_DEST, THIRD_DEST]]
     return np.concatenate((outs_scores, runners_on_float), axis=1)
+
+def play_score(outs_scores: np.ndarray):
+    runner_dests = outs_scores[:, [4, 5, 6 ,7]]
+    runner_dests[runner_dests <= 3] = 0
+    runner_dests[runner_dests > 3] = 1
+    outs_scores[:, 4] = np.sum(runner_dests, axis=1)
+    return outs_scores
+
+def get_game_scores():
+    games = np.genfromtxt(os.path.join(BASE_DATA_PATH, '2019GAME.csv'), delimiter=',', dtype=int)
+    scores = games[:, 1] + games[:, 2]
+    return scores
 
 if __name__ == '__main__':
     pass
